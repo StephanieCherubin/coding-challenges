1c2379e5698c3d224049a0ed249e1d6143637e26
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2018-10-25 16:58:59 -0700

    update files to better

--- python_quote.py
+++ python_quote.py
@@ -17,7 +17,7 @@ quotes = ("It's just a flesh wound.",
           "THIS IS AN EX-PARROT!!")
 
 def random_python_quote():
-    rand_index = random.randint(0, len(quotes) - 1)
+    rand_index = random.randrange(len(quotes) - 1)
     return quotes[rand_index]
 
 if __name__ == '__main__':
--- rearrange.py
+++ rearrange.py
@@ -1,25 +1,14 @@
-import sys
+# Replace the string you want to shuffle with myString
 import random
+
 
 myString = "how now brown cow"
 
-def Convert(string):
-    myList = list(string.split(" "))
-    random.shuffle(myList)
+def convert(string):
+    myList = list(string.split())
+    random.randrange(len(myList))
     return myList
 
 if __name__ == '__main__':
-    answer = ' '.join(Convert(myString))
-    print(answer)
-
-# import random
-#
-# quotes = ["how"]["now"]["brown"]["cow"]
-#
-# def random_python_quote():
-#     rand_index = random.randint(0, len(quotes) - 1)
-#     return quotes[rand_index]
-#
-# if __name__ == '__main__':
-#     quote = random_python_quote()
-#     print(quote)
+    print(' '.join(convert(myString)))
+    # print(rearrange)
