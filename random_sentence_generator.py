ba75c20f0b2ffc640a61242466dba835a3b8c83e
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2018-11-15 22:27:37 -0800

    edit files

---
+++ random_sentence_generator.py
@@ -0,0 +1,14 @@
+# use a corpus to pull information
+# Count word frequency
+# Get percentage/decimal
+# loop through each word
+# have a counter.
+# add percentage of word to counter.
+# choose words at random based on their count.
+# more common words would be more likely to occur.
+
+def fname(arg):
+    text_file = open("alice.txt", "r")
+
+    text_file.close()
+
