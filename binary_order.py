86c06ad1362f2d4d84fac89c92bb5755d70b1bee
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2019-01-08 11:30:27 -0500

    [add] binary search tree file

---
+++ binary_order.py
@@ -0,0 +1,18 @@
+# class BinarySearchTree(object):
+    # def __init__(self, arg):
+    #     self.arg = arg
+
+class Node:
+    def __init__(self, data):
+        self.left = None
+        self.right = None
+        self.data = data
+
+    def insert(self, data):
+        if data < self.data:
+            if self.left is not None:
+                self.left = Node(data)
+            else:
+                self.left.append(data)
+
+
