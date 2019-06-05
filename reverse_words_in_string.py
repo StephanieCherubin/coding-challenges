7165d2c3d0984a57e620702487501267d0ccc634
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2019-06-05 09:56:33 -0700

    add coding problem

---
+++ reverse_words_in_string.py
@@ -0,0 +1,29 @@
+'''Given a string, you need to reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.'''
+
+
+
+'''Buy and Sell Stock
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+
+Note that you cannot sell a stock before you buy one.'''
+
+
+'''Example 1:
+Input: [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+             Not 7-1 = 6, as selling price needs to be larger than buying price.
+'''
+
+'''
+Example 2:
+Input: [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transaction is done, i.e. max profit = 0.
+'''
+
+
+if input[right_index] -input[left_input] > max_profit
+  max_profit = new_profit
