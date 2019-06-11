7972dc0b39824575bfeb27a66fdaa2abf65e6875
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2019-06-11 08:40:21 -0700

    add file

---
+++ python-spd.py
@@ -0,0 +1,10 @@
+def findT(arr, t):
+  for num in arr:
+    i = 1
+    sum = arr[num] + arr[num + i]
+    if sum == t:
+      return (arr[num], arr[num + i])
+    else:
+      i += 1
+
+print(findT([11, 7, 3, 9, 17], 16))
