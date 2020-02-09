f0fa441b4c6e6f8be1155ddf0ed6395144d0eead
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2020-02-08 23:33:58 -0800

    add comments

--- push_zeroes_to_end.py
+++ push_zeroes_to_end.py
@@ -7,17 +7,20 @@ def pushZeroesToEnd(arr, n):
 	#Traverse the array. If element encountered is non-zero, then replace the element at index 'count' with this element
 	for num in range(len(arr)):
 		if arr[num] != 0:
+    			
+    		# here count is incremented
 			arr[count] = arr[num]
 			count += 1
 					
+	# Now all non-zero elements have been shifted to front and 'count is set as index of first 0. Make all elements 0 from count to end
 	while count < len(arr):
 		arr[count] = 0
 		count += 1
 
-arr = [1, 9, 8, 4, 0, 0, 2, 7, 0, 6, 0, 9]
-n = len(arr)
-pushZeroesToEnd(arr, n)
-print(f'Array after pushing all zeros to end of array: {arr}')
+if __name__ == "__main__":
+	arr = [1, 1, 3, 4, 24, 0, 10, 2, -7, 47, 6, 0, 9]
+	pushZeroesToEnd(arr, len(arr))
+	print(f'Array after pushing all zeros to end of array: {arr}')
 			
 
 
