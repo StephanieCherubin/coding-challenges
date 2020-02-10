1a9d32e7da0261bfb59352f1f2ccd0800e65cf39
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2020-02-09 23:23:13 -0800

    change function for easier

--- push_zeroes_to_end.py
+++ push_zeroes_to_end.py
@@ -1,25 +1,15 @@
 # Python3 code to move all zeroes at the end of array
 
 # Function that pushes all zeroes to end of an array
-def pushZeroesToEnd(arr, n):
-	count = 0 # Count of non-zero elements
-
-	#Traverse the array. If element encountered is non-zero, then replace the element at index 'count' with this element
-	for num in range(len(arr)):
-		if arr[num] != 0:
-    			
-    		# here count is incremented
-			arr[count] = arr[num]
-			count += 1
-					
-	# Now all non-zero elements have been shifted to front and 'count is set as index of first 0. Make all elements 0 from count to end
-	while count < len(arr):
-		arr[count] = 0
-		count += 1
+def pushZeroesToEnd(arr):
+	for num in arr:
+		if num == 0:
+			arr.remove(num)
+			arr.append(num)
 
 if __name__ == "__main__":
-	arr = [1, 1, 3, 4, 24, 0, 10, 2, -7, 47, 6, 0, 9]
-	pushZeroesToEnd(arr, len(arr))
+	arr = [0, 1, 1, 3, 4, 24, 0, 0, 0, 10, 2, -7, 47, 6, 0, 9]
+	pushZeroesToEnd(arr)
 	print(f'Array after pushing all zeros to end of array: {arr}')
 			
 
