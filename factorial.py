454360d437d73a67735297d03650fb062adea12e
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2020-02-09 23:30:26 -0800

    [add] if name == main

--- factorial.py
+++ factorial.py
@@ -1,4 +1,8 @@
 def factorial(n):
-    if n < 2 :
-		return 1
-	return n * factorial(n-1)
+    	
+		if n < 2 :
+			return 1
+		return n * factorial(n-1)
+
+if __name__ == "__main__":
+	print(factorial(5))
