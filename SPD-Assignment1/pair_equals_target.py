b4c7bd339a126d9d5bdebb042374b12651b4ff39
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2020-04-24 14:37:22 -0400

    change sum to t

--- SPD-Assignment1/pair_equals_target.py
+++ SPD-Assignment1/pair_equals_target.py
@@ -3,20 +3,17 @@
     # Input: a=[5, 3, 6, 8, 2, 4, 7], t=10
     # Ouput:  [3, 7] or [6, 4] or [8, 2]
 
-def pairEqualsTarget(a, t):
-    pass
-
-def printPairs(arr, sum): 
+def pairEqualsTarget(arr, t):
       
     # Create an empty hash set 
     s = set()
       
     for i in range(0, len(arr)): 
-        pair = sum-arr[i] 
+        pair = t-arr[i] 
         if (pair in s): 
             print(arr[i], pair)
         s.add(arr[i]) 
   
 A = [5, 3, 6, 8, 2, 4, 7] 
 n = 10
-printPairs(A, n) 
+pairEqualsTarget(A, n) 
