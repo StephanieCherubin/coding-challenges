7823d935c24673b57b7ea87ac5b7f1b956c2dbb9
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2020-04-24 14:41:38 -0400

    add this file

--- python-spd.py
+++ python-spd.py
@@ -2,7 +2,7 @@ def findT(arr, t):
   for num in arr:
     i = 1
     sum = arr[num] + arr[num + i]
-    if sum == t:
+    if sum == t:  
       return (arr[num], arr[num + i])
     else:
       i += 1
