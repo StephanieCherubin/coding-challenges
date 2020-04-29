4e720b9ea793e920079eeba04a6c9368f4c38277
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2020-04-29 08:18:19 -0400

    add a main function

--- factorial.py
+++ factorial.py
@@ -1,8 +1,12 @@
+'''Write a recursive function that returns the factorial of a given number'''
+
 def factorial(n):
     	
-		if n < 2 :
-			return 1
-		return n * factorial(n-1)
-
+	if n < 2 :
+		return 1
+	return n * factorial(n-1)
+	
+def main():
+    	print(factorial(5))
 if __name__ == "__main__":
-	print(factorial(5))
+	main()
