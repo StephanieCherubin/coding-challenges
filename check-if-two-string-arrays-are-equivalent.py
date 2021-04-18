ac52115950781f8189caff906653fc1ea4449498
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2021-04-17 23:29:52 -0700

    add check-if-two-string-arrays-are-equivalent from leetcode

---
+++ check-if-two-string-arrays-are-equivalent.py
@@ -0,0 +1,22 @@
+def arrayStringsAreEqual(word1, word2):
+    "Given two string arrays word1 and word2, return true if the two arrays represent the same string, and false otherwise. A string is represented by an array if the array elements concatenated in order forms the string."
+    
+    # concatenate all strings for word1
+    word1_concat = "".join(word1)
+    
+    # concatenate all strings for word2
+    word2_concat = "".join(word2)
+  
+    #  if word1 == word2:
+    if word1_concat == word2_concat:
+        return True
+    else:
+        return False
+        # return true
+    # else return false
+print(arrayStringsAreEqual(word1 = ["ab", "c"], word2 = ["a", "bc"]))
+# Output: true
+print(arrayStringsAreEqual(word1 = ["a", "cb"], word2 = ["ab", "c"]))
+# Output: false
+print(arrayStringsAreEqual(word1  = ["abc", "d", "defg"], word2 = ["abcddefg"]))
+# Output: true  
