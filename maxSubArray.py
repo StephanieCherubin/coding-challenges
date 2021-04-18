5c75fd1473d6be4a937d38b7f742841444c26bf2
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2021-04-17 23:34:08 -0700

    edit function

--- maxSubArray.py
+++ maxSubArray.py
@@ -5,15 +5,19 @@ def maxSubArray(nums):
   max_sum = 0
 
   for i in nums:
+    if len(nums) == 1:
+          return nums
     current_max_sum += i
     if max_sum < current_max_sum:
       max_sum = current_max_sum
-    if current_max_sum < 0:
+    elif current_max_sum < 0:
       current_max_sum = 0
   return max_sum
 
-print(maxSubArray([-2,1,-3,4,-1,2,1,-500,900]))
-print(maxSubArray([-2, -3, 4, -1, -2, 1, 5, -3]))
-print(maxSubArray([-2, 1, -3, 4]))
-print(maxSubArray([-2, -5, 6, -2, -3, 1, 5, -6]))
-print(maxSubArray([-2,-1, -3, 4, -1, 2, 1]))
+if __name__ == '__main__':
+  print(maxSubArray([-1]))
+  # print(maxSubArray([-2,1,-3,4,-1,2,1,-500,900]))
+  # print(maxSubArray([-2, -3, 4, -1, -2, 1, 5, -3]))
+  # print(maxSubArray([-2, 1, -3, 4]))
+  # print(maxSubArray([-2, -5, 6, -2, -3, 1, 5, -6]))
+  # print(maxSubArray([-2,-1, -3, 4, -1, 2, 1]))
