5f78c8ba743395450b183a05db6263f01ad86e97
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2021-04-22 09:02:57 -0700

    Add docstring and problem

--- set-mismatch.py
+++ set-mismatch.py
@@ -1,22 +1,20 @@
 from collections import Counter
 
 def findErrorNums(nums):
-    # '''You have a set of integers s, which originally contains all the numbers from 1 to n. Unfortunately, due to some error, one of the numbers in s got duplicated to another number in the set, which results in repetition of one number and loss of another number.
-    # You are given an integer array nums representing the data status of this set after the error.
-    # Find the number that occurs twice and the number that is missing and return them in the form of an array.'''
+    '''You have a set of integers s, which originally contains all the numbers from 1 to n. Unfortunately, due to some error, one of the numbers in s got duplicated to another number in the set, which results in repetition of one number and loss of another number.
+    You are given an integer array nums representing the data status of this set after the error.
+    Find the number that occurs twice and the number that is missing and return them in the form of an array.'''
     
     nums = sorted(nums)
     numerical = list(range(1, max(nums)+1))
 
     print('numerical:', numerical)
     print('nums:', nums)
-    # output = []
-    # for i in numerical:
-    #     if i not in nums:
-    #         output.append(i)
-    # print(output)
-    
-              
+    output = []
+    for i in numerical:
+        if i not in nums:
+            output.append(i)
+    print(output)
             
     
 # print(findErrorNums(nums = [1,2,2,4])) # Output: [2,3]
